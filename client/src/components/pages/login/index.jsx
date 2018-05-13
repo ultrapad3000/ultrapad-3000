7bedd6cc216c60a7931291bbc7c389d33e195c18
Nazarov MI <m@nazarov-mi.ru>
2018-05-13 19:32:36 +0300

    feature: добавит заготовки для страниц входа и регистрации, для компонента input

---
+++ client/src/components/pages/login/index.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Input from '@/components/reusable/input'
+
+const constructor = () => (
+  <div>
+    Login Page
+    <Input />
+  </div>
+)
+
+export default constructor
---
+++ client/src/components/reusable/input/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './index.module.sass'
+
+const constructor = () => (
+  <div className={ styles.root }>
+    Input
+  </div>
+)
+
+export default constructor
